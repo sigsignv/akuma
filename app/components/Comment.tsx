5b98a297dba4fa725b2050a81455363db5d3281f
Sigsign <sig@signote.cc>
2025-03-27 11:46:42 +0900

    Move icons to the left side

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -12,17 +12,21 @@ export type CommentProps = {
 
 export default function Comment({ userId, name, icon, createdAt, comment, link }: CommentProps) {
   return (
-    <div>
-      <div className="flex items-center gap-1">
+    <div className="flex gap-2">
+      <div className="flex-shrink-0">
         <Icon src={icon} alt={`${userId}'s profile icon`} />
-        <span className="text-sm font-bold">{userId}</span>
-        <span className="text-sm text-gray-600">
-          <a href={link}>
-            <ElapsedTime date={createdAt} locale="ja" />
-          </a>
-        </span>
       </div>
-      <div className="ml-6">{comment}</div>
+      <div className="flex-grow">
+        <div className="flex items-center gap-2">
+          <span className="text-sm font-bold">{userId}</span>
+          <span className="text-sm text-gray-600">
+            <a href={link}>
+              <ElapsedTime date={createdAt} locale="ja" />
+            </a>
+          </span>
+        </div>
+        <div>{comment}</div>
+      </div>
     </div>
   );
 }
