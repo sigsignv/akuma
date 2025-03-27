4f5f52b5fc019f4b4a665dbe39853834f75bb3d9
Sigsign <sig@signote.cc>
2025-03-27 18:52:47 +0900

    Refactor Comment component

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -19,16 +19,26 @@ export default function Comment({ author, content, createdAt, link }: Comment) {
   return (
     <div className="flex gap-2">
       <div className="flex-shrink-0">
-        <Icon src={author.icon} alt={`${author.name ?? author.id}'s profile icon`} />
+        <a href={author.link} target="_blank" rel="noreferrer">
+          <Icon src={author.icon} alt={`${author.name ?? author.id}'s profile icon`} />
+        </a>
       </div>
       <div className="flex-grow">
-        <div className="flex items-center gap-2">
-          <span className="text-sm font-bold">{author.id}</span>
-          <span className="text-sm text-gray-600">
-            <a href={link}>
+        <div className="flex items-center gap-1">
+          {!author.name ? (
+            <span className="text-sm font-bold">{author.id}</span>
+          ) : (
+            <>
+              <span className="text-sm font-bold">{author.name}</span>
+              <span className="text-sm text-gray-600">@{author.id}</span>
+            </>
+          )}
+          <span className="text-sm text-gray-600">・</span>
+          <a href={link} target="_blank" rel="noreferrer">
+            <span className="text-sm text-gray-600 no-underline hover:underline">
               <ElapsedTime date={createdAt} locale="ja" />
-            </a>
-          </span>
+            </span>
+          </a>
         </div>
         <div>{content}</div>
       </div>
