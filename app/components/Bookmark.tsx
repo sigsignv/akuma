2de93830fd1c9ebc6d7d2ce4812f564bbb262261
Sigsign <sig@signote.cc>
2025-05-30 23:24:34 +0900

    Refactor Bookmark component

--- app/components/Bookmark.tsx
+++ app/components/Bookmark.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Await, useAsyncValue } from "react-router";
+import { Await, useAsyncError, useAsyncValue } from "react-router";
 import type { getBookmark } from "~/routes/api.bookmark";
 import ElapsedTime from "./ElapsedTime";
 import Icon from "./Icon";
@@ -12,106 +12,130 @@ type BookmarkProps = {
 };
 
 export default function Bookmark({ promise, url }: BookmarkProps) {
+  const props: BaseProps = {
+    fallbackUrl: generateFallbackUrl(url),
+  };
+
+  return (
+    <React.Suspense fallback={<BookmarkSkeleton {...props} />}>
+      <Await resolve={promise} errorElement={<BookmarkError {...props} />}>
+        <BookmarkComments {...props} />
+      </Await>
+    </React.Suspense>
+  );
+}
+
+type BaseProps = {
+  fallbackUrl: string;
+};
+
+type ViewProps = BaseProps & {
+  title?: string;
+  link?: string;
+  children: React.ReactNode;
+};
+
+function BookmarkView({ title, link, children, fallbackUrl }: ViewProps) {
   return (
     <details id="bookmark" className="pt-4 pb-4" open={true}>
       <summary className="text-2xl font-bold cursor-pointer select-none">
-        <React.Suspense fallback="はてなブックマーク">
-          <Await resolve={promise}>
-            <BookmarkTitle />
-          </Await>
-        </React.Suspense>
+        {title ? title : "はてなブックマーク"}
       </summary>
-      <div>
-        <React.Suspense fallback={<li>Loading...</li>}>
-          <Await resolve={promise} errorElement={<li>Error</li>}>
-            <BookmarkComments />
-          </Await>
-        </React.Suspense>
-      </div>
+      <div>{children}</div>
       <div className="flex justify-center pt-4">
-        <React.Suspense fallback={<BookmarkLink link={generateLink(url)} />}>
-          <Await resolve={promise} errorElement={<BookmarkLink link={generateLink(url)} />}>
-            {(bookmark) =>
-              bookmark?.entry_url ? (
-                <BookmarkLink link={bookmark.entry_url} />
-              ) : (
-                <BookmarkLink link={generateLink(url)} />
-              )
-            }
-          </Await>
-        </React.Suspense>
+        <a href={link ? link : fallbackUrl} target="_blank" rel="noreferrer">
+          <span className="text-sm text-gray-600 dark:text-gray-400 hover:underline">
+            はてなブックマークを見る
+          </span>
+        </a>
       </div>
     </details>
   );
 }
 
-function BookmarkTitle() {
-  const b = useAsyncValue() as Bookmarks;
-
-  const comments = b.bookmarks.length;
-  const total = b.count;
-
+function BookmarkSkeleton(props: BaseProps) {
   return (
-    <span>
-      はてなブックマーク ({comments} / {total})
-    </span>
+    <BookmarkView {...props}>
+      <div className="flex justify-center items-center">
+        <p className="m-4">Loading...</p>
+      </div>
+    </BookmarkView>
   );
 }
 
-function BookmarkComments() {
-  const { bookmarks, eid } = useAsyncValue() as Bookmarks;
-
-  if (bookmarks.length === 0) {
-    return <span className="text-gray-500">No comments</span>;
-  }
+function BookmarkError(props: BaseProps) {
+  const error = useAsyncError() as Error;
 
   return (
-    <ul className="space-y-4">
-      {bookmarks.map((c) => (
-        <li key={c.user} className="space-y-2">
-          <div className="flex gap-2">
-            <div className="flex-shrink-0">
-              <a href={`https://b.hatena.ne.jp/${c.user}/`} target="_blank" rel="noreferrer">
-                <Icon
-                  src={`https://cdn.profile-image.st-hatena.com/users/${c.user}/profile.png`}
-                  alt={`${c.user}'s profile icon`}
-                />
-              </a>
-            </div>
-            <div className="flex-grow">
-              <div className="flex items-center gap-1">
-                <span className="text-sm font-bold">{c.user}</span>
-                <span className="text-sm text-gray-600">・</span>
-                <a
-                  href={`https://b.hatena.ne.jp/entry/${eid}/comment/${c.user}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <span className="text-sm text-gray-600 no-underline hover:underline">
-                    <ElapsedTime date={c.timestamp} locale="ja" />
-                  </span>
-                </a>
+    <BookmarkView {...props}>
+      <div className="flex justify-center items-center">
+        <p className="m-4">{error.message}</p>
+      </div>
+    </BookmarkView>
+  );
+}
+
+function BookmarkComments({ fallbackUrl }: BaseProps) {
+  const bookmarks = useAsyncValue() as Bookmarks;
+
+  const b = bookmarks ?? {
+    entry_url: fallbackUrl,
+    count: 0,
+    bookmarks: [],
+    eid: "0",
+  };
+
+  const props = {
+    fallbackUrl,
+    title: `はてなブックマーク (${b.bookmarks.length} / ${b.count})`,
+    link: b.entry_url,
+  };
+
+  return (
+    <BookmarkView {...props}>
+      {b.bookmarks.length === 0 ? (
+        <div className="flex justify-center items-center">
+          <p className="m-4">コメントはありません</p>
+        </div>
+      ) : (
+        <ul className="space-y-4">
+          {b.bookmarks.map((c) => (
+            <li key={c.user} className="space-y-2">
+              <div className="flex gap-2">
+                <div className="flex-shrink-0">
+                  <a href={`https://b.hatena.ne.jp/${c.user}/`} target="_blank" rel="noreferrer">
+                    <Icon
+                      src={`https://cdn.profile-image.st-hatena.com/users/${c.user}/profile.png`}
+                      alt={`${c.user}'s profile icon`}
+                    />
+                  </a>
+                </div>
+                <div className="flex-grow">
+                  <div className="flex items-center gap-1">
+                    <span className="text-sm font-bold">{c.user}</span>
+                    <span className="text-sm text-gray-600">・</span>
+                    <a
+                      href={`https://b.hatena.ne.jp/entry/${b.eid}/comment/${c.user}`}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <span className="text-sm text-gray-600 no-underline hover:underline">
+                        <ElapsedTime date={c.timestamp} locale="ja" />
+                      </span>
+                    </a>
+                  </div>
+                  <div>{c.comment}</div>
+                </div>
               </div>
-              <div>{c.comment}</div>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+            </li>
+          ))}
+        </ul>
+      )}
+    </BookmarkView>
   );
 }
 
-function BookmarkLink({ link }: { link: string }) {
-  return (
-    <a href={link} target="_blank" rel="noreferrer">
-      <span className="text-sm text-gray-600 dark:text-gray-400 hover:underline">
-        はてなブックマークを見る
-      </span>
-    </a>
-  );
-}
-
-function generateLink(url: string): string {
+function generateFallbackUrl(url: string): string {
   const escapedUrl = url.replaceAll(/#/g, "%23");
   return `https://b.hatena.ne.jp/entry/${escapedUrl}`;
 }
