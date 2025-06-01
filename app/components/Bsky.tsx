b7f7fbd0886d1f67828faf5577d50c9b778e3a01
Sigsign <sig@signote.cc>
2025-06-01 21:24:43 +0900

    Refactor Bsky by merging List and Comment into a single component

--- app/components/Bsky.tsx
+++ app/components/Bsky.tsx
@@ -1,5 +1,8 @@
+import React from "react";
+import { Await } from "react-router";
+import ElapsedTime from "~/components/ElapsedTime";
+import Icon from "~/components/Icon";
 import type { getBskyPost } from "../routes/api.bsky";
-import List from "./List";
 
 type BlueskyProps = {
   promise: ReturnType<typeof getBskyPost>;
@@ -10,7 +13,38 @@ export default function Bsky({ promise }: BlueskyProps) {
     <details id="bluesky" className="pt-4 pb-4" open={true}>
       <summary className="text-2xl font-bold cursor-pointer select-none">Bluesky</summary>
       <div className="flex justify-center items-center">
-        <List promise={promise} />
+        <React.Suspense fallback={<li>Loading...</li>}>
+          <Await resolve={promise} errorElement={<li>Error</li>}>
+            {({ comments }) => (
+              <ul className="space-y-4">
+                {comments.map((c) => (
+                  <li key={c.link} className="space-y-2">
+                    <div className="flex gap-2">
+                      <div className="flex-shrink-0">
+                        <a href={c.author.link} target="_blank" rel="noreferrer">
+                          <Icon src={c.author.icon} alt={`${c.author.name}'s profile icon`} />
+                        </a>
+                      </div>
+                      <div className="flex-grow">
+                        <div className="flex items-center gap-1">
+                          <span className="text-sm font-bold">{c.author.name}</span>
+                          <span className="text-sm text-gray-600">@{c.author.id}</span>
+                          <span className="text-sm text-gray-600">・</span>
+                          <a href={c.link} target="_blank" rel="noreferrer">
+                            <span className="text-sm text-gray-600 no-underline hover:underline">
+                              <ElapsedTime date={c.createdAt} locale="ja" />
+                            </span>
+                          </a>
+                        </div>
+                        <div>{c.content}</div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </Await>
+        </React.Suspense>
       </div>
     </details>
   );
