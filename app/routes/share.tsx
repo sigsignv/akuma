1a4edd068dc6c71da947135b59b74a8d15a3acd8
Sigsign <sig@signote.cc>
2025-12-03 14:26:37 +0900

    Simplify share route to redirect to peek

--- app/routes/share.tsx
+++ app/routes/share.tsx
@@ -1,26 +1,11 @@
 import { redirect } from "react-router";
 import type { Route } from "./+types/share";
 
+/**
+ * @deprecated use "peek" route instead
+ */
 export function loader({ request }: Route.LoaderArgs) {
-  const u = new URL(request.url);
-  const text = u.searchParams.get("text");
-  const url = u.searchParams.get("url");
+  const url = new URL(request.url);
 
-  if (url && URL.canParse(url)) {
-    throw redirectToReactions(url);
-  }
-
-  if (text) {
-    const regexp = /(?:^|\s+)(?<url>https?:\/\/[^\s]+)$/u;
-    const url = text.match(regexp)?.groups?.url;
-    if (url && URL.canParse(url)) {
-      throw redirectToReactions(url);
-    }
-  }
-
-  throw redirect("/");
-}
-
-function redirectToReactions(url: string) {
-  return redirect(`/reactions?url=${encodeURIComponent(url)}`);
+  throw redirect(`/peek?${url.searchParams.toString()}`);
 }
