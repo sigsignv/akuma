90d730bda87c7a90aae5d51888a3fa9b05dcd31f
Sigsign <sig@signote.cc>
2026-01-07 13:55:27 +0900

    Extract page layout

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import type { RouteConfig } from "@react-router/dev/routes";
-import { index, route } from "@react-router/dev/routes";
+import { index, layout, route } from "@react-router/dev/routes";
 
 export default [
   index("./routes/welcome.tsx"),
-  route("peek", "./routes/peek.tsx"),
+  layout("./pages/layout.tsx", [route("peek", "./routes/peek.tsx")]),
 ] satisfies RouteConfig;
--- app/viewer/Viewer.tsx
+++ app/viewer/Viewer.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigation } from "react-router";
+import { useNavigation } from "react-router";
 import Bsky from "~/components/Bsky";
 import HackerNews from "~/components/HackerNews";
 import SearchBar from "~/components/SearchBar";
@@ -21,43 +21,21 @@ export default function Viewer({ url, bookmark, posts, news }: Props) {
   const navigation = useNavigation();
 
   return (
-    <div className="container mx-auto max-w-xl">
-      <div className="flex flex-col mx-2 min-h-dvh md:min-h-auto">
-        <header className="p-4">
-          <h1 className="text-2xl font-bold hover:underline">
-            <Link to="/">Akuma</Link>
-          </h1>
-        </header>
+    <div>
+      <div className="px-4">
+        <SearchBar url={url} />
+      </div>
 
-        <main className="grow pt-4 bg-white dark:bg-zinc-900 rounded-lg shadow-md">
-          <div className="px-4">
-            <SearchBar url={url} />
-          </div>
-
-          <div style={{ opacity: navigation.state === "loading" ? 0.5 : 1.0 }}>
-            <div className="px-2 my-4">
-              <Bookmark promise={bookmark} url={url} />
-            </div>
-            <div className="px-2 my-4">
-              <Bsky promise={posts} url={url} />
-            </div>
-            <div className="px-2 my-4">
-              <HackerNews promise={news} url={url} />
-            </div>
-          </div>
-        </main>
-
-        <footer className="p-4">
-          <div className="flex justify-center items-center text-zinc-600 dark:text-zinc-400">
-            <a
-              href="https://github.com/sigsignv/akuma"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <span className="iconify mdi--github text-4xl hover:text-zinc-800 dark:hover:text-zinc-300" />
-            </a>
-          </div>
-        </footer>
+      <div style={{ opacity: navigation.state === "loading" ? 0.5 : 1.0 }}>
+        <div className="px-2 my-4">
+          <Bookmark promise={bookmark} url={url} />
+        </div>
+        <div className="px-2 my-4">
+          <Bsky promise={posts} url={url} />
+        </div>
+        <div className="px-2 my-4">
+          <HackerNews promise={news} url={url} />
+        </div>
       </div>
     </div>
   );
