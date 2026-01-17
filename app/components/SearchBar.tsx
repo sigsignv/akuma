3865a91c3bb8c8372772f1eaac8bd0814b4f2797
Sigsign <sig@signote.cc>
2026-01-17 18:52:39 +0900

    Prevent iOS Safari zoom on focus by removing text-sm class

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -10,14 +10,14 @@ export default function SearchBar({ url }: Props) {
       <Form method="get" action="/peek">
         <div className="relative">
           <input
-            className="w-full text-sm border rounded-md pl-3 pr-12 py-2 focus:border-transparent"
+            className="w-full border rounded-md pl-3 pr-12 py-2 focus:border-transparent"
             type="url"
             name="url"
             defaultValue={url}
             placeholder="e.g., https://example.com/"
           />
           <button
-            className="absolute top-1 right-1 p-1 border-3 border-transparent rounded flex items-center justify-center text-white bg-zinc-800 dark:bg-zinc-600 focus:outline-none hover:bg-zinc-700 active:bg-zinc-600 dark:hover:bg-zinc-500 dark:active:bg-zinc-400 select-none"
+            className="absolute top-1 right-1 p-1 border-5 border-transparent rounded flex items-center justify-center text-white bg-zinc-800 dark:bg-zinc-600 focus:outline-none hover:bg-zinc-700 active:bg-zinc-600 dark:hover:bg-zinc-500 dark:active:bg-zinc-400 select-none"
             type="submit"
           >
             <span className="iconify mdi--arrow-right-thick h-4 w-4" />
